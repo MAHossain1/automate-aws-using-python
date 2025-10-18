0444a1fa9979f37d1f85847f8aff400cf1f59595
MAHossain1 <mahpro110@gmail.com>
2025-10-18 11:28:40 +0600

    volumes backup scheduling done.

---
+++ volume-backups.py
@@ -0,0 +1,24 @@
+import boto3
+import schedule
+
+ec2_client = boto3.client("ec2", region_name="ap-south-1")
+
+
+def create_volume_snapshots():
+    volumes = ec2_client.describe_volumes(
+        Filters=[{"Name": "tag:Name", "Values": ["prod"]}]
+    )
+    for volume in volumes["Volumes"]:
+        print(
+            f"VolumeId: {volume['VolumeId']}, Size: {volume['Size']} GiB, State: {volume['State']}, Type: {volume['VolumeType']}"
+        )
+        new_snapshot = ec2_client.create_snapshot(VolumeId=volume["VolumeId"])
+        print(
+            f"\tCreated snapshot {new_snapshot['SnapshotId']} for volume {volume['VolumeId']}"
+        )
+
+
+schedule.every(5).day.at("01:00").do(create_volume_snapshots)
+
+while True:
+    schedule.run_pending()
